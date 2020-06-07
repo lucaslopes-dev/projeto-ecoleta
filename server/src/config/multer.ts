340be716600c85f6ef97b1a6cc996788e82fd55f
Lucas Lopes <lucaslopes.js@hotmail.com>
2020-06-06 22:11:21 -0300

    projeto finalizado

---
+++ server/src/config/multer.ts
@@ -0,0 +1,16 @@
+import multer, { diskStorage } from "multer";
+import path from "path";
+import crypto from "crypto";
+
+export default multer({
+  storage: diskStorage({
+    destination: path.resolve(__dirname, "..", "..", "uploads"),
+    filename(req, file, callback) {
+      const hash = crypto.randomBytes(16).toString("hex");
+
+      const fileName = `${hash}-${file.originalname}`;
+
+      callback(null, fileName);
+    },
+  }),
+});
